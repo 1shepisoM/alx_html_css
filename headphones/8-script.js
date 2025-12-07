32365d4bfba0b784d72c134c9e838bc7bb8b08ba
Tshepiso <espanyolmarutlulle@gmail.com>
2025-12-07 19:32:06 +0200

    Add Headphones folder with README

---
+++ headphones/8-script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger-btn');
+const mobileNav = document.getElementById('mobile-nav');
+
+// Toggle menu
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    mobileNav.classList.toggle('show');
+});
